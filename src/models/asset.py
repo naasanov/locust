91330bdc8fa9cc1ba8a6209b755373ac90804f02
VidurShah <vidurshah12@gmail.com>
2026-02-22 01:33:13 -0500

    added submodule nuclei-templates and also implemented exp-02

--- src/models/asset.py
+++ src/models/asset.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime, timezone
 from typing import Literal
 
@@ -31,6 +32,7 @@ class CloudIssue(BaseModel):
 
 
 class AssetDocument(BaseModel):
+    asset_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     engagement_id: str
     asset_type: AssetType
     ip: str | None = None
